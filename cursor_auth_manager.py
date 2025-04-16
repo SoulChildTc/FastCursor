d1c28b890c94f1657f289c791d96bf9602a74335
soulchildtc <742899387@qq.com>
2025-04-16 19:38:15 +0800

    refactor(cursor_auth_manager): 优化登录流程中的等待机制
    将固定等待时间替换为动态检查登录状态的循环，以提高登录流程的可靠性

--- cursor_auth_manager.py
+++ cursor_auth_manager.py
@@ -116,9 +116,38 @@ def login_cursor(email, password):
     tab.actions.click("@name=password").input(password)
     tab.actions.click("@type=submit")
 
-    time.sleep(1)
-    
+
+    # 检查是否登录成功
+    while True:
+        if tab.ele("@type=submit"):
+            time.sleep(0.5)  # 等待页面加载
+            continue
+        else:
+            break
+            
     handle_turnstile(tab)
+
+    # # # 检查是否需要处理验证码, 尝试 2 次
+    # for _ in range(2):
+    #     try:
+    #         if tab.ele("@data-index=0"):
+    #             logging.info("正在获取邮箱验证码...")
+    #             code = EmailVerificationHandler(email).get_verification_code()
+    #             if not code:
+    #                 logging.error("获取验证码失败")
+    #                 return False
+
+    #             logging.info(f"成功获取验证码: {code}")
+    #             logging.info("正在输入验证码...")
+    #             i = 0
+    #             for digit in code:
+    #                 tab.ele(f"@data-index={i}").input(digit)
+    #                 time.sleep(random.uniform(0.1, 0.3))
+    #                 i += 1
+    #             logging.info("验证码输入完成")
+    #             break
+    #     except Exception as e:
+    #         logging.error(f"验证码处理过程出错: {str(e)}")
 
     # tab.get(settings_url)
     # try:
